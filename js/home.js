d719423968e00ef05a04ff63a37c25fa4d2e90d3
natcaires <nataly.caires@gmail.com>
2020-01-10 22:24:36 -0700

    First Commit

---
+++ js/home.js
@@ -0,0 +1,5 @@
+let year = new Date().getFullYear();
+document.getElementById('year').innerHTML = year;
+
+let date = document.lastModified;
+document.getElementById('lastUpdate').innerHTML = "Last Updated: " + date;
