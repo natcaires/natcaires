38b86a433bc8148ca977c9693428188c5b2e87cb
natcaires <nataly.caires@gmail.com>
2020-02-01 23:18:14 -0700

    fixed footer and JS

--- lesson4/js/preston-4.js
+++ lesson4/js/preston-4.js
@@ -1,16 +1,26 @@
+//Toggle Menu function.
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
 
 
-
-
-
-
-
-
+//Copyright Year
 let year = new Date().getFullYear();
 document.getElementById('year').innerHTML = year;
 
-let date = document.lastModified;
-document.getElementById('lastUpdate').innerHTML = "Last Updated: " + date;
+
+//Current Date
+let today = new Date();
+
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
+let currentWeekday = days[today.getDay()]; 
+
+let date = today.getDate(); 
+
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]; 
+
+let currentMonth = months[today.getMonth()]; 
+
+let currentYear = today.getFullYear(); 
+
+document.getElementById("todaysDate").innerHTML = currentWeekday + ", " + date + " " + currentMonth + " " + currentYear;
