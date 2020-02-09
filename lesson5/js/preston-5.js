d1a57b298c86451f14a946eb73e88b8771edd37a
natcaires <nataly.caires@gmail.com>
2020-02-08 21:09:49 -0700

    lesson 5 finished

--- lesson5/js/preston-5.js
+++ lesson5/js/preston-5.js
@@ -1,3 +1,17 @@
+//Friday Message.
+let dayToday = new Date();
+let dayOfWeek = dayToday.getDay();
+
+if (dayOfWeek == 5){
+    let message = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+    document.getElementById("hiddenMessage").innerHTML = message;
+    document.getElementById("hiddenMessage").style.backgroundColor = "#F7D660";
+    document.getElementById("hiddenMessage").style.textAlign = "center";
+    document.getElementById("hiddenMessage").style.padding = "0.8em";
+    document.getElementById("hiddenMessage").style.fontFamily = "Roboto, sans-serif";
+  }
+
+
 //Toggle Menu function.
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
